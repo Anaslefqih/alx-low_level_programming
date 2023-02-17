8e875430a60fe639bb0f2849931c9cd016f2bedd
anas lefqih <anas.lefqih@ump.ac.ma>
2023-02-17 09:46:25 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print all single digits in base 10
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
