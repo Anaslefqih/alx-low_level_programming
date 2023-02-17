6186a41989757c3d3850976e5d63948ffe4b2d3b
anas lefqih <anas.lefqih@ump.ac.ma>
2023-02-17 10:35:58 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+##include <srdio.h>
+/**
+ * main - print num in base 16
+ * Return 0 (success)
+ */
+int main(void)
+{
+	int n;
+	char alp;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+	for (alp = 'a'; alp <= 'f'; alp++)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
