4fb161e65482e3f1a11bd589e9a199dff5b11291
anas lefqih <anas.lefqih@ump.ac.ma>
2023-02-21 18:53:45 -0800

    16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * reverse_string - function that reverses the order of a string
+ * This function takes a string and reverses the order of its characters
+ * in place and returns the reversed string.
+ *
+ * @str: the string to be reversed
+ * Return: pointer to the reversed string
+*/
+char *reverse_string(char *str)
+{
+	char *left = str;
+	char *right = str + strlen(str) - 1;
+	char temp;
+
+	while (right > left)
+	{
+		temp = *right;
+		*right = *left;
+		*left = temp;
+		right--;
+		left++;
+	}
+	return (str);
+}
+
