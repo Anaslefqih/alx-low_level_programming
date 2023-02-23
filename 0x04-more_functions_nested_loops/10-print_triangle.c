4ed24d47da15fac0421b9921ee88b9987f68d4e0
anas lefqih <anas.lefqih@ump.ac.ma>
2023-02-23 14:03:53 -0800

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,6 +11,10 @@ void print_triangle(int size)
 
 	if (size <= 0)
 	{
+		_putchar('\n');
+	}
+	else
+	{
 		for (i = 1; i <= size; i++)
 		{
 			for (j = 1; j <= size - i; j++)
@@ -23,9 +27,5 @@ void print_triangle(int size)
 			}
 			_putchar('\n');
 		}
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
