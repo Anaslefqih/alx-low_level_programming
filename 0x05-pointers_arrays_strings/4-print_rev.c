23a0905524ba9600c106598917c8198bc9cca6ec
anas lefqih <anas.lefqih@ump.ac.ma>
2023-02-27 07:24:46 -0800

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,15 @@
 void print_rev(char *s)
 {
 	int L = 0;
+	int a;
 
 	while (s[L] != '\0')
 	{
 		L++;
 	}
-	for (L; L > 0; L--)
+	for (a = L; a > 0; a--)
 	{
 		_putchar(s[L]);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
