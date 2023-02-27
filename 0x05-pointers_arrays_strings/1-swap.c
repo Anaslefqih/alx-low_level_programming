6b1a39f8851e695415209c4fba20867a8036b5cf
anas lefqih <anas.lefqih@ump.ac.ma>
2023-02-27 06:49:13 -0800

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: integer to be swaped.
+ * @b: integer to be swaped.
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
