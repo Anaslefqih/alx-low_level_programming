13481f4a4d9db915e8c70507e6f63692b4465b2c
anas lefqih <anas.lefqih@ump.ac.ma>
2023-02-27 08:37:41 -0800

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: The destination
+ * @src: The source
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a]; a++)
+	{
+		dest[i] = src[a];
+	}
+	dest[a] = '\0';
+	return (dest);
+}
