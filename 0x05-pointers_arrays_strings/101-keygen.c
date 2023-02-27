6db48bdf2e9a1dd55cdfe1d95a1ab17058da1205
anas lefqih <anas.lefqih@ump.ac.ma>
2023-02-27 09:31:43 -0800

    101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int password[100];
+	int i, m, n;
+
+	m = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		password[i] = rand() % 78;
+		m += (password[i] + '0');
+		putchar(password[i] + '0');
+		if ((2000 - m) - '0' < 78)
+		{
+			n = 2000 - m - '0';
+			m += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
+
