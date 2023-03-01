9688a36cae9254307ba1a09ec52d1ee16bd13c7c
anas lefqih <anas.lefqih@ump.ac.ma>
2023-03-01 12:24:08 -0800

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers
+ * @n: number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (i < n - 1)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+		i++;
+	}
+}
