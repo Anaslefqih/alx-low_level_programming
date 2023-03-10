bdf3d097d7aa94076f2e55703a597e985325f897
anas lefqih <anas.lefqih@ump.ac.ma>
2023-03-10 15:33:03 -0800

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - prints all arguments it receives
+* @argc: number of arguments
+* @argv: array of arguments	
+* Return: 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		return (printf("Error\n"), 1);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
