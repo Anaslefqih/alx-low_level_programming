703db504f36cf893185bb75a923b6829009ae56d
anas lefqih <anas.lefqih@ump.ac.ma>
2023-03-16 06:47:57 -0700

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,6 +12,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *result;
 	unsigned int i;
+
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
